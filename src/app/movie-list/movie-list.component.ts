3b61cf84db4f2c6b90066ccce733e23fe8e60ed3
Aidan Starr <aidan.starr19@gmail.com>
2020-07-22 21:11:54 -0400

    Api working

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiResponseService } from '../api-response.service';
+import { MovieParent } from '../api-interfaces'
 
 @Component({
   selector: 'app-movie-list',
@@ -10,11 +11,12 @@ export class MovieListComponent implements OnInit {
 
   constructor(private api: ApiResponseService){}
 
-    movies: object;
+  movies: any;
 
   ngOnInit(): void {
-    this.api.getMovies().subscribe((data) => this.movies = {...data})
-  }
+    this.api.getMovies().subscribe((data: MovieParent) =>{
+      this.movies = data.results})
+    }
   
 
 }
