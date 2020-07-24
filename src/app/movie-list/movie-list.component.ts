c57bcd87567f30aa14614db5367b8fbceb2c21bc
Aidan Starr <aidan.starr19@gmail.com>
2020-07-23 21:54:22 -0400

    search working

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -13,11 +13,12 @@ export class MovieListComponent implements OnInit {
   constructor(private api: ApiResponseService){}
 
   movies: any[];
+  searchResults: any[];
   endPointURL: string='discover/movie?api_key=ab96898a4ea60dd2468dcd8ae39dd30c&page=';
   currentPage: number= 1;
   imagePath: string= "https://image.tmdb.org/t/p/w600_and_h900_bestv2/";
-  updateDisplay(){
-    this.movies = this.movies;
+  searched(event){
+    this.movies = event;
   }
   nextPage(){
     this.currentPage++;
--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { ApiResponseService } from '../api-response.service';
 import { MovieParent } from '../api-interfaces';
 
@@ -15,10 +15,11 @@ export class SearchCriteriaComponent implements OnInit {
   }
   endPointURL: string='search/movie?api_key=ab96898a4ea60dd2468dcd8ae39dd30c&query=';
   query: string;
-  searchResults: any;
+  @Output() onSearch = new EventEmitter<object>();
+  @Input() searchResults: any;
   getSearch(f) {
     this.api.getMovies(this.endPointURL,f.value.query).subscribe((data: MovieParent) =>{
-      this.searchResults = data.results;
+      this.onSearch.emit(data.results);
     })
   }
 
