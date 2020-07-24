d128d2b6b437dbfdf9ccf3b85f053d4bb694491d
Doug Chu <doug@Dougs-MBP.attlocal.net>
2020-07-23 21:52:55 -0400

    second commmit: movie detail component working, now implementing in-app navigation

--- src/app/api-response.service.ts
+++ src/app/api-response.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MovieParent } from './api-interfaces';
+import { Movie } from './api-interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiResponseService {
 
+  movies: Movie[];
+
   constructor(private http: HttpClient) { }
   apiURL: string= 'https://api.themoviedb.org/3/discover/movie?api_key=ab96898a4ea60dd2468dcd8ae39dd30c'
   getMovies(){
     return this.http.get(`${this.apiURL}`)
   }
+
+  // getMovie(id: number) {
+  //   return of(Movies.find(movie => movie.id === id));
+  // }
+  //do I need to call on the API movie id property to route links properly?
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -3,6 +3,8 @@ import { ApiResponseService } from '../api-response.service';
 import { MovieParent } from '../api-interfaces';
 import { Movie } from '../api-interfaces';
 import { SearchCriteriaComponent } from '../search-criteria/search-criteria.component';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-movie-list',
@@ -11,14 +13,32 @@ import { SearchCriteriaComponent } from '../search-criteria/search-criteria.comp
 })
 export class MovieListComponent implements OnInit {
 
-  constructor(private api: ApiResponseService){}
+  constructor(
+    private api: ApiResponseService,
+    private route: ActivatedRoute,
+    private location: Location
+  ){}
+
   imagePath: string = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/"
 
-  movies: any;
+  movies: Movie[];
+  // movies: any; (Aidan's original code)
   // image: string;"https://image.tmdb.org/t/p/w600_and_h900_bestv2/" 
 
+  // selectedMovie: Movie;
+  // onSelect(movie: Movie): void {
+  //   this.selectedMovie = movie;
+  // }
+
   ngOnInit(): void {
-    this.api.getMovies().subscribe((data: MovieParent) =>{
-      this.movies = data.results})
+    this.api.getMovies().subscribe((data: MovieParent) => {
+    this.movies = data.results});
     }
+
+  getMovie(): void {
+    // this.api.getMovies().subscribe((data: MovieParent) =>{
+    //   this.movies = data.results});
+    const id = +this.route.snapshot.paramMap.get('id');
+    // this.api.getMovies().subscribe(movie => this.movie = movie);
+  }
 }
